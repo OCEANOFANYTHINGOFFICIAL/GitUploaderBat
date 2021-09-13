d8a1593d19bd3fa45b71760c45e888bda37ec239
oceanofanything <oceanofanything@gmail.com>
2021-09-13 20:37:02 +0530

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -1,13 +1,28 @@
 @echo off
 color 1f
-title Git File Uploader Bot
+title Git Large File System Uploader Bot
 set /p dir=Give Location Of Uploading Directory: 
 echo.
 echo Uploading Directory:  %dir%
 echo.
+set /p opti=add README.md ?: 
+if %opti% == yes goto opt-yes
+if %opti% == no goto oth
+if %opti% == Yes goto opt-yes
+if %opti% == No goto oth
+if %opti% == YES goto opt-yes
+if %opti% == NO goto oth
+echo.
+:opt-yes
 set /p rme=Write Your Readme.md Here In One Line: 
 echo.
 echo Readme.md:  %rme%
+echo.
+echo # %rme% >> README.md
+:oth
+set /p ext=Mention Which File Types (extentions or names) Will Be Tracked With LFS. Type(*.*) To Track All: 
+echo.
+echo File Will Be Tracked : %rme%
 echo.
 set /p com=Write Your Commit Message Here: 
 echo.
@@ -19,8 +34,9 @@ echo Remote Url:  %remurl%
 echo.
 set /p branch=Set Your Git Branch: 
 cd %dir%
-echo # %rme% >> README.md
 git init
+git lfs track "%ext%"
+git add .gitattributes
 git add .
 git commit -m "%com%"
 git branch -M %branch%
